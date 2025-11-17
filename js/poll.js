b1c94efc09eaac339264d0f87147dedb30863611
Joe Yu <65329073+Tibyliz@users.noreply.github.com>
2025-11-17 18:45:24 +0900

    Update poll.js

--- js/poll.js
+++ js/poll.js
@@ -50,12 +50,12 @@ function generateDates() {
 }
 
 // Format date as MM-DD
-//function formatDate(date) {
-//    const year = date.getFullYear();
-//    const month = String(date.getMonth() + 1).padStart(2, '0');
-//    const day = String(date.getDate()).padStart(2, '0');
-//    return `${month}-${day}`;
-//}
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${month}-${day}`;
+}
 
 // Get day name
 function getDayName(date) {
